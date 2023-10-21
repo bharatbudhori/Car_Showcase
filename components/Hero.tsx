aefd5bc0845a13f33b5ec786aff121e81c9ef24a
bharatbudhori <70325600+bharatbudhori@users.noreply.github.com>
2023-10-21 09:48:23 +0530

    hero and layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
 import React from "react";
 import Image from "next/image";
-import CustomButton from "./CustomButton";
+import { CustomButton } from ".";
 
 const Hero = () => {
     const handleScroll = () => {
@@ -12,24 +12,38 @@ const Hero = () => {
         });
     };
 
-    return <div className="hero">
-        <div className="flex-1 pt-36 padding-x">
-            <h1 className="hero__title">
-                Find, book, or rent a car - quickly and easily!
-            </h1>
+    return (
+        <div className="hero">
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className="hero__title">
+                    Find, book, or rent a car - quickly and easily!
+                </h1>
 
-            <p className="hero__subtitle">
-                Streamline your car rental experience with our effortless booking process.
-            </p>
+                <p className="hero__subtitle">
+                    Streamline your car rental experience with our effortless
+                    booking process.
+                </p>
 
-            <CustomButton 
-                title = "Explore Cars"
-                containerStyles = "bg-primary-blue text-white rounded-full mt-10"
-                handleClick={handleScroll}
+                <CustomButton
+                    title="Explore Cars"
+                    containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                    handleClick={handleScroll}
+                />
+            </div>
+            <div className="hero__image-container">
+                <div className="hero__image">
+                    <Image
+                        src="/hero.png"
+                        alt="hero"
+                        fill
+                        className="object-contain"
+                    />
+                </div>
 
-            />
+                <div className="hero__image-overlay"></div>
+            </div>
         </div>
-    </div>;
+    );
 };
 
 export default Hero;
